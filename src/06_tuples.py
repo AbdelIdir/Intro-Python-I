6f7a63537cf9ac98d187d446825835e0b768da10
AbdelI <homeworkoracle@protonmail.com>
2020-04-06 15:27:39 +0200

    finished tuples file

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -31,23 +31,23 @@ import math
 # print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
 
-    
 
 t = (1, 2, 5, 7, 99)
 
-def print_tuple(t):
-    for data in t:
+
+def print_tuple(a_tuple):
+    for data in a_tuple:
         print(data)
+
 
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
 
-
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
+
 print_tuple(u)
