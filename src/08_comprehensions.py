9ec15f476f5e72bc62dc3c563ff1c757c649fb93
AbdelI <homeworkoracle@protonmail.com>
2020-04-06 16:03:33 +0200

    comprehension to produce cubes, wirtten

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,13 +11,16 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
-print (y)
+y.extend([1, 2, 3, 4, 5])
+print(y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+
+y = [num * num * num for num in range(10)]
+
 
 print(y)
 
