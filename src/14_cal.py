1ab5013d0950ee7c7859488599b26058a840e87d
AbdelI <homeworkoracle@protonmail.com>
2020-04-07 18:48:10 +0200

    calendar now ouputs the current month when no ouput

--- src/14_cal.py
+++ src/14_cal.py
@@ -6,14 +6,16 @@ https://docs.python.org/3.6/library/calendar.html
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
+
+
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
  - If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
- - If the user specifies two arguments, assume they passed in
-   both the month and the year. Render the calendar for that
-   month and year.
+
+
+
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
@@ -32,25 +34,48 @@ import calendar
 from datetime import datetime
 
 userType = input("Please type something: ")
+userType = ""
 # userType2 = input("Please type something here: ")
 
-GeneralUserInput = userType.split(" ")
+# GeneralUserInput = userType.split(" ")
 
 # print(int(GeneralUserInput[2]), int(GeneralUserInput[3]))
 # print(GeneralUserInput)
 
-theyear = GeneralUserInput[2]
-themonth = GeneralUserInput[3]
+# theyear = GeneralUserInput[2]
+# themonth = GeneralUserInput[3]
 
-# now = datetime.now()
-# print(now.strftime("%Y-%m-%d"))
+now = datetime.now()
+# print(now.strftime("%m"))
+
+currentMonth = now.strftime("%m")
+currentYear = now.strftime("%Y")
+
+
 # print(now)
 # print(userType)
 
 # This prints the calendar of the specified year and month.
 
+# - If the user specifies two arguments, assume they passed in
+# both the month and the year. Render the calendar for that   <<<< DONE
+# month and year.
+
 c = calendar.TextCalendar(calendar.MONDAY)
+# stuff = c.formatmonth(int(themonth), int(theyear))
 
-stuff = c.formatmonth(int(themonth), int(theyear))
+stuff2 = c.formatmonth(int(currentYear), int(currentMonth))
 
-print(stuff)
+print(stuff2)
+# print(stuff if len(GeneralUserInput) > 3 else now)
+
+# - If the user doesn't specify any input, your program should
+# print the calendar for the current month. The 'datetime'
+# module may be helpful for this.
+
+
+# print(stuff if len(GeneralUserInput) > 4 else stuff2)
+
+
+# - If the user specifies one argument, assume they passed in a
+#   month and render the calendar for that month of the current year.   <<<<<
