f4aa5de0636ca3fbce00df430074b00b3d6f9f85
AbdelI <homeworkoracle@protonmail.com>
2020-04-08 15:02:10 +0200

    edited calendar

--- src/14_cal.py
+++ src/14_cal.py
@@ -6,16 +6,14 @@ https://docs.python.org/3.6/library/calendar.html
 Write a program that accepts user input of the form
   `14_cal.py [month] [year]`
 and does the following:
-
-
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
  - If the user specifies one argument, assume they passed in a
    month and render the calendar for that month of the current year.
-
-
-
+ - If the user specifies two arguments, assume they passed in
+   both the month and the year. Render the calendar for that
+   month and year.
  - Otherwise, print a usage statement to the terminal indicating
    the format that your program expects arguments to be given.
    Then exit the program.
@@ -33,46 +31,17 @@ import sys
 import calendar
 from datetime import datetime
 
-# userType = input("Please type something: ")
-# userType = ""  for when user inputs nothing
 
-# GeneralUserInput = userType.split(" ")
-
-# print(int(GeneralUserInput[2]), int(GeneralUserInput[3]))
-# print(GeneralUserInput)
-
-# themonth = GeneralUserInput[2]
-# themonth = sys.argv[1]
-
-now = datetime.now()
-# print(now.strftime("%m"))
-
-currentMonth = now.strftime("%m")
-currentYear = now.strftime("%Y")
-
-# - If the user specifies two arguments, assume they passed in
-# both the month and the year. Render the calendar for that   <<<< DONE
-# month and year.
-
-c = calendar.TextCalendar(calendar.MONDAY)
-# stuff = c.formatmonth(int(themonth), int(theyear))
-
-stuff2 = c.formatmonth(int(currentYear), int(currentMonth))
-
-# - If the user specifies one argument, assume they passed in a
-#   month and render the calendar for that month of the current year.   <<<<<
-
-
-def calendarPro(year= sys.argv[1:2][0] , month=sys.argv[2:3][0]):
-
-    stuff3 = c.formatmonth(int(month), int(year))
-    print(stuff3)
-
-# print(sys.argv[1:2])
-# ['12']
-
-
-calendarPro(currentYear, currentMonth if len(sys.argv) < 2 else False)
-
-
-# print(sys.argv[1:2][0], sys.argv[2:3])
+x = datetime.now()
+year = x.year
+month = x.month
+monthInput = input('Enter a month to view:')
+yearInput = input('Enter a year to:')
+if (not monthInput or not yearInput):
+    print(calendar.month(year, month))
+elif (monthInput and not yearInput):
+    print(calendar.month(year, int(monthInput)))
+elif (monthInput and yearInput):
+    print(calendar.month(int(yearInput), int(monthInput)))
+else:
+    print('please provide year and month')
